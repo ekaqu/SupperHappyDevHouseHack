65b0008c099780062db8c2b4ce09a491a964955d
David Capwell <ekaqu1028@gmail.com>
2011-07-09 17:47:31 -0700

    got the frontend to replace text area with div

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 http.createServer(function (request, response) {
+  console.log('new connection');
+  response.write('Welcome');
   request.on('data', function(chunk) {
+    console.log(chunk);
     response.write(chunk);
-    response.end(); // only for testing
+    response.end();
   });
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
