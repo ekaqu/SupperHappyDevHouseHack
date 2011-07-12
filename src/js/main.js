989fedf48cbb8edcb9ed032fe255736b525f16e8
David Capwell <ekaqu1028@gmail.com>
2011-07-11 22:33:23 -0700

    centered the map on all sides.  polling less offtten.  when server gives an id, insert the element into the page

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,18 @@
 YUI().use('json','node', 'io', 'querystring-stringify-simple', 'querystring', function(Y) {
-  var map = Y.one('.brian-map');
+  var map = Y.one('.brian-map'),
+    child, 
+    largestId = 0;
 
   // when user clicks, add an input
   map.on('click', function(e) {
-    var child = Y.Node.create('<input class="brian-map-element" style="left: '+e.clientX+'px; top: '+e.clientY+'px" type="text"></input>');
+    // if user clicks someplace else, remove the last input
+    if(child) child.remove();
+    child = Y.Node.create('<input class="brian-map-element" style="left: '+e.clientX+'px; top: '+e.clientY+'px" type="text"></input>');
     map.append(child);
     child.focus();
     child.on('keypress', function(press) {
       if(press.keyCode == 13) { // if spacebar
         var text = child.get('value');
-        // map.append('<div class="brian-map-element" style="left: '+e.clientX+'px; top: '+e.clientY+'px">'+text+'</div>');
         child.remove();
 
         // call backend
@@ -22,26 +25,28 @@ YUI().use('json','node', 'io', 'querystring-stringify-simple', 'querystring', fu
           },
           on: {
             complete: function(id,rsp,err) {
-              Y.log(id);
-              Y.log(rsp.responseText);
-              Y.log(err);
+              largestId = rsp.responseText;
+              insertElement(map,e.clientX,e.clientY,text);
             }
           }
         });
       }
     });
   });
+
+  function insertElement(map, x, y, word) {
+    map.append('<div class="brian-map-element" style="left: '+ x +'px; top: '+ y +'px">'+ word +'</div>');
+  }
   
   // fetch data and post it on the site
-  var largestId = 0;
   function reloadAll() {
     Y.io('server/backend.php?from=' + largestId, {
       on : {
         complete: function(id, rsp, err) {
           var words = Y.JSON.parse(rsp.responseText);
+          Y.log(words);
           Y.each(words, function(word) {
-            Y.log(word);
-            map.append('<div class="brian-map-element" style="left: '+word.x+'px; top: '+word.y+'px">'+word.word+'</div>');
+            insertElement(map,word.x,word.y,word.word);
             largestId = word.id;
           });
           Y.log(largestId);
@@ -49,5 +54,6 @@ YUI().use('json','node', 'io', 'querystring-stringify-simple', 'querystring', fu
       }
     });
   }
-  setInterval(reloadAll, 1000);
+  reloadAll();
+  setInterval(reloadAll, 5000);
 });
